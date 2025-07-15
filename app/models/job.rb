eef88177d3401df5a8be81d1cf98b2256192364d
neb <jneb.dev@outlook.com>
2025-07-14 21:23:00 -0600

    adds validate presence to job model, adds tests to job model rspec

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,3 @@
 class Job < ApplicationRecord
+  validates :title, :description, :company, :location, presence: true
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    job = Job.new(title: "Dev", description: "Code things", company: "Acme",
+                  location: "Remote")
+    expect(job).to be_valid
+  end
+
+  it "is invalid without a title" do
+    job = Job.new(title: nil)
+    expect(job).not_to be_valid
+  end
 end
