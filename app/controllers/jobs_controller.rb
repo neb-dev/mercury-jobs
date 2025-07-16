ea4ffdd19d72f03988887d4059a678d29e34bc82
neb <jneb.dev@outlook.com>
2025-07-15 23:06:44 -0600

    adds edit, update, and job_params to jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,22 @@ class JobsController < ApplicationController
   def new
     @job = Job.new()
   end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to(@job, notice: "Job updated successfully.")
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def job_params
+      params.require(:job).permit(:title, :description, :company, :location)
+    end
 end
